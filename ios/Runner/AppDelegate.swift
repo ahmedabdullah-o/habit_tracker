c93d8f94d4bb656b51ba1d27576ab6da39230bf3
EliTE709 <logames4freehack@gmail.com>
2025-07-09 00:32:53 +0300

    added permission_handler

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -18,16 +18,3 @@ import flutter_local_notifications
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-// @UIApplicationMain
-// override func application(
-//   _ application: UIApplication,
-//   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//   ) -> Bool {
-//   // This is required to make any communication available in the action isolate.
-//   FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-//     GeneratedPluginRegistrant.register(with: registry)
-//   }
-
-//   ...
-//   return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-// }
