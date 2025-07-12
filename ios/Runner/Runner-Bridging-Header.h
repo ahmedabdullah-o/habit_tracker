a25d40e4d9e1639d78eacb76b8028c24e4cd4c93
EliTE709 <logames4freehack@gmail.com>
2025-07-12 13:04:54 +0300

    fixed linting issues

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,5 +1,1 @@
-// Copyright 2025 Ahmed Abdullah
-#ifndef IOS_RUNNER_RUNNER_BRIDGING_HEADER_H_
-#define IOS_RUNNER_RUNNER_BRIDGING_HEADER_H_
 #import "GeneratedPluginRegistrant.h"
-#endif  // IOS_RUNNER_RUNNER_BRIDGING_HEADER_H_
