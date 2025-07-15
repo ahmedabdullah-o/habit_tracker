77ae354f3a2b54f11ab99529ee2ab1234db41338
EliTE709 <logames4freehack@gmail.com>
2025-07-15 19:33:24 +0300

    defined notification_model

--- lib/core/services/notifications/notification_model.dart
+++ lib/core/services/notifications/notification_model.dart
@@ -1,1 +1,17 @@
-class NotificationModel {}
+class NotificationModel {
+  int id;
+  String title;
+  String body;
+  String channelName;
+  String? subtext;
+  String? payload;
+
+  NotificationModel(
+    this.id,
+    this.title,
+    this.body, {
+    this.channelName = 'habit',
+    this.subtext,
+    this.payload,
+  });
+}
